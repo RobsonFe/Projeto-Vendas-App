0b6666aca116d7a0070451e7bae91e6c32ad1183
RobsonFe <robson_20ferreira@hotmail.com>
2023-09-16 22:02:16 -0300

    Formulario com campos label corrigidos e funcoes de manipulacao de estado incluidas

--- src/components/produtos/cadastro/index.tsx
+++ src/components/produtos/cadastro/index.tsx
@@ -1,14 +1,33 @@
+import { useState } from 'react'
 import { Layout } from "components"
 
 export const CadastroProdutos: React.FC = () =>{
+
+    const [nome, setNome] = useState('')
+    const [preco, setPreco] = useState('')
+    const [sku, setSku] = useState('')
+    const [descricao, setDescricao] = useState('')
+
+    const submit = () =>{
+
+        let produtos = {
+
+            nome: nome,
+            preco: preco,
+            sku: sku,
+            descricao: descricao
+        }
+
+        console.log(produtos)
+    }
 
     return(
         <Layout titulo="Cadastro de Produtos">
             <div className="columns">
             <div className="field column is-full">
-                <label className="label" >Nome: *</label>
-                <div htmlFor="inputNome" className="control">
-                <input className="input" id="inputNome" type="text" placeholder="Digite o Nome do Produto" required></input>
+                <label className="label" htmlFor="inputNome" >Nome: *</label>
+                <div className="control">
+                <input className="input" id="inputNome" value={nome} onChange={ event => setNome(event.target.value)} type="text" placeholder="Digite o Nome do Produto" required></input>
                 </div>
                 
             </div>
@@ -17,17 +36,17 @@ export const CadastroProdutos: React.FC = () =>{
 
             <div className="columns">
             <div className="field is-half column">
-                <label className="label" >Preço: *</label>
-                <div htmlFor="inputPreco" className="control">
-                <input className="input" id="inputPreco" type="number" placeholder="Digite o Preco do Produto" required></input>
+                <label className="label" htmlFor="inputPreco" >Preço: *</label>
+                <div  className="control">
+                <input className="input" id="inputPreco" value={preco} onChange={ event => setPreco( event.target.value)} type="text" placeholder="Digite o Preco do Produto" required></input>
                 </div>
                 
             </div>
 
             <div className="field is-half column">
-                <label className="label" >SKU: *</label>
-                <div htmlFor="inputSKU" className="control">
-                <input className="input" id="inputSKU" type="text" placeholder="Digite o SKU do Produto" required></input>
+                <label className="label" htmlFor="inputSKU"  >SKU: *</label>
+                <div className="control">
+                <input className="input" id="inputSKU" value={sku} onChange={ event => setSku( event.target.value)} type="text" placeholder="Digite o SKU do Produto" required></input>
                 </div>
                 
             </div>
@@ -35,9 +54,9 @@ export const CadastroProdutos: React.FC = () =>{
             
             <div className="columns">
             <div className="field column is-full">
-                <label className="label" >Descrição: *</label>
-                <div htmlFor="inputDesc" className="control">
-                <textarea className="textarea" id="inputDesc" placeholder="Digite a Descrição do Produto" required></textarea>
+                <label className="label" htmlFor="inputDesc" >Descrição: *</label>
+                <div  className="control">
+                <textarea className="textarea" id="inputDesc" value={descricao} onChange={ event => setDescricao( event.target.value)} placeholder="Digite a Descrição do Produto" required></textarea>
                 </div>
                 
             </div>
@@ -45,7 +64,7 @@ export const CadastroProdutos: React.FC = () =>{
 
             <div className="field is-grouped">
             <div className="control">
-            <button className="button is-link">Salvar</button>
+            <button className="button is-link" onClick={submit}>Salvar</button>
             <button className="button is-link is-light">Voltar</button>
             </div>
             </div>
