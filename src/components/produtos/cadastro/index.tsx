97a0169711ee430fcc86d2ba8fff9f4a9a194ad5
RobsonFe <robson_20ferreira@hotmail.com>
2023-09-16 21:03:44 -0300

    Campos de formularios criados

--- src/components/produtos/cadastro/index.tsx
+++ src/components/produtos/cadastro/index.tsx
@@ -4,7 +4,44 @@ export const CadastroProdutos: React.FC = () =>{
 
     return(
         <Layout titulo="Cadastro de Produtos">
-            Aqui vai ficar o meu formulario
+            <div className="field">
+                <label className="label" >Nome: *</label>
+                <div htmlFor="inputNome" className="control">
+                <input className="input" id="inputNome" type="text" placeholder="Digite o Nome do Produto" required></input>
+                </div>
+                
+            </div>
+
+            <div className="field">
+                <label className="label" >Preço: *</label>
+                <div htmlFor="inputPreco" className="control">
+                <input className="input" id="inputPreco" type="number" placeholder="Digite o Preco do Produto" required></input>
+                </div>
+                
+            </div>
+
+            <div className="field">
+                <label className="label" >SKU: *</label>
+                <div htmlFor="inputSKU" className="control">
+                <input className="input" id="inputSKU" type="text" placeholder="Digite o SKU do Produto" required></input>
+                </div>
+                
+            </div>
+
+            <div className="field">
+                <label className="label" >Descrição: *</label>
+                <div htmlFor="inputDesc" className="control">
+                <textarea className="textarea" id="inputDesc" placeholder="Digite a Descrição do Produto" required></textarea>
+                </div>
+                
+            </div>
+
+            <div className="field is-grouped">
+            <div className="control">
+            <button className="button is-link">Salvar</button>
+            <button className="button is-link is-light">Voltar</button>
+            </div>
+            </div>
         </Layout>
     )
 }
