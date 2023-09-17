f2f9768b12580c68371d493ef60741bf030a772c
RobsonFe <robson_20ferreira@hotmail.com>
2023-09-16 21:18:01 -0300

    alinhamento dos campos

--- src/components/produtos/cadastro/index.tsx
+++ src/components/produtos/cadastro/index.tsx
@@ -4,15 +4,19 @@ export const CadastroProdutos: React.FC = () =>{
 
     return(
         <Layout titulo="Cadastro de Produtos">
-            <div className="field">
+            <div className="columns">
+            <div className="field column is-full">
                 <label className="label" >Nome: *</label>
                 <div htmlFor="inputNome" className="control">
                 <input className="input" id="inputNome" type="text" placeholder="Digite o Nome do Produto" required></input>
                 </div>
                 
             </div>
+            </div>
+            
 
-            <div className="field">
+            <div className="columns">
+            <div className="field is-half column">
                 <label className="label" >Preço: *</label>
                 <div htmlFor="inputPreco" className="control">
                 <input className="input" id="inputPreco" type="number" placeholder="Digite o Preco do Produto" required></input>
@@ -20,20 +24,23 @@ export const CadastroProdutos: React.FC = () =>{
                 
             </div>
 
-            <div className="field">
+            <div className="field is-half column">
                 <label className="label" >SKU: *</label>
                 <div htmlFor="inputSKU" className="control">
                 <input className="input" id="inputSKU" type="text" placeholder="Digite o SKU do Produto" required></input>
                 </div>
                 
             </div>
-
-            <div className="field">
+            </div>
+            
+            <div className="columns">
+            <div className="field column is-full">
                 <label className="label" >Descrição: *</label>
                 <div htmlFor="inputDesc" className="control">
                 <textarea className="textarea" id="inputDesc" placeholder="Digite a Descrição do Produto" required></textarea>
                 </div>
                 
+            </div>
             </div>
 
             <div className="field is-grouped">
