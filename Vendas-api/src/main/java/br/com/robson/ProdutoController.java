301426cf6c03aad3c3b14b3186f2e289c3e30191
RobsonFe <robson_20ferreira@hotmail.com>
2023-09-20 01:24:41 -0300

    adicionando requisicao POST

---
+++ Vendas-api/src/main/java/br/com/robson/ProdutoController.java
@@ -0,0 +1,19 @@
+package br.com.robson;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Controlador da operação
+@RequestMapping("/api/produtos") // Request para localizar o mapeamento da pagina/ a rota.
+public class ProdutoController {
+		
+		@PostMapping // recebenfdo o metodo post de uma requisição
+		public ProdutoFormRequest salvar(@RequestBody ProdutoFormRequest produto) {
+			System.out.println(produto);
+			return produto;
+			
+		} // RequestBody convertendo um arquivo Json em String vindo do corpo da requisição. 
+		//Classe ProdutoFormRequest sendo instanciada para receber os dados do Front-End como parametro. 
+}
